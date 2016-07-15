1b514c4f026bd26e2690b4f714a195d81ef5aaa9
luke.darham <luke.darham@gmail.com>
2016-07-15 13:13:03 +0100

    remove client urls

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     proxy = require('express-http-proxy');
 
-var api = process.env.API || 'http://cbre-search-dev.cloudapp.net', 
-    cdn = process.env.CDN || 'https://uatlistingssearchcbreeun.blob.core.windows.net',
-    cms = process.env.CMS || 'http://54.163.251.114',
-    host = process.env.HOST || 'http://cbre-int.clients.amido.com';
+var api = process.env.API || '', 
+    cdn = process.env.CDN || '',
+    cms = process.env.CMS || '',
+    host = process.env.HOST || '';
 
 // API Calls to API
 app.use('/api/*', proxy(api, {
@@ -25,7 +25,9 @@ app.use('/resources*', proxy(cdn, {
 // Everything else to Drupal
 app.use('/*', proxy(cms, {
   forwardPath: function(req, res) {
-    return host + req.originalUrl;
+    var fullurl = host + req.originalUrl;
+    console.log(fullurl);
+    return fullurl;
   }
 }));
 
